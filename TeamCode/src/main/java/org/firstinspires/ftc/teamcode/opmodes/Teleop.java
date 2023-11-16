03c23b4edcb8aa4893570c8b55acfc8ce1577369
MetaInfinityFTC <metainfinity18221@gmail.com>
2023-11-15 19:59:05 -0500

    Intake Testing
    Testing code for intake lololol yes sirr=e===e idj if ti wikk worj or not
    
    Co-Authored-By: polargh <89708970+polargh@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
@@ -1,4 +1,48 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
-public class Teleop {
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.normalizeRadians;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+@Config
+@TeleOp(name = "\uD83D\uDC80\"")
+public class Teleop extends LinearOpMode {
+    //get our analog input from the hardwareMap
+    AnalogInput analogInput;// = hardwareMap.get(AnalogInput.class, "miniaxon");
+    DcMotor intake;
+    Servo intakeservo;
+
+    // get the voltage of our analog line
+// divide by 3.3 (the max voltage) to get a value between 0 and 1
+// multiply by 360 to convert it to 0 to 360 degrees
+    double position;
+    public void runOpMode(){
+        intakeservo = hardwareMap.servo.get("intakeservo");
+        analogInput = hardwareMap.analogInput.get("miniaxon");
+        intake = hardwareMap.dcMotor.get("intake");
+        waitForStart();
+        while(opModeIsActive()) {
+            intakeservo.setPosition(0);
+            position = analogInput.getVoltage() / 3.3 * 360;
+            intake.setPower(0.8);
+            telemetry.addData("angle:", position);
+            telemetry.update();
+        }
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/intake/ActiveIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/intake/ActiveIntake.java
@@ -1,4 +1,38 @@
 package org.firstinspires.ftc.teamcode.subsystem.intake;
 
-public class ActiveIntake {
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.normalizeRadians;
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.CommandScheduler;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class ActiveIntake extends LinearOpMode{
+    //get our analog input from the hardwareMap
+    AnalogInput analogInput = hardwareMap.get(AnalogInput.class, "miniaxon");
+    Servo intakeservo = hardwareMap.servo.get("intakeservo");
+
+    // get the voltage of our analog line
+// divide by 3.3 (the max voltage) to get a value between 0 and 1
+// multiply by 360 to convert it to 0 to 360 degrees
+    double position = analogInput.getVoltage() / 3.3;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //intakeservo.setPosition(0);
+        telemetry.addData("angle", analogInput);
+    }
 }
