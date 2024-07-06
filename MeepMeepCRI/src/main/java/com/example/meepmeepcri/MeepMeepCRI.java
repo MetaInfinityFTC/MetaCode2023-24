b02a9455b00039d795722a9035344cbaed10ce3f
Arav Maheria <dashforever1808@gmail.com>
2024-07-06 00:23:40 -0400

    made meep meep changes, the view is a bit squished so the entire field can be seen

--- MeepMeepCRI/src/main/java/com/example/meepmeepcri/MeepMeepCRI.java
+++ MeepMeepCRI/src/main/java/com/example/meepmeepcri/MeepMeepCRI.java
@@ -7,28 +7,28 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepCRI {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+            MeepMeep meepMeep = new MeepMeep(1365, 700);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .build()
-                );
+            RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                    // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                    .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                    .followTrajectorySequence(drive ->
+                            drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                                    .forward(30)
+                                    .turn(Math.toRadians(90))
+                                    .forward(30)
+                                    .turn(Math.toRadians(90))
+                                    .forward(30)
+                                    .turn(Math.toRadians(90))
+                                    .forward(30)
+                                    .turn(Math.toRadians(90))
+                                    .build()
+                    );
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
-                .setDarkMode(true)
-                .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
-                .start();
-    }
+            meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK_CRI)
+                    .setDarkMode(true)
+                    .setBackgroundAlpha(0.95f)
+                    .addEntity(myBot)
+                    .start();
+        }
 }
