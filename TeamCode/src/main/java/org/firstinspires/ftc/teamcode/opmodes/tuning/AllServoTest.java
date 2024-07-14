b877777e60997615e7950b3c359f7b9106e091e4
Arav Maheria <dashforever1808@gmail.com>
2024-07-13 20:18:47 -0400

    finished TeleCRI

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/AllServoTest.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.opmodes.tuning;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Robot;
+
+@TeleOp
+@Config
+public class AllServoTest extends LinearOpMode {
+
+    public static double clawL = 0;
+    public static double clawR = 0;
+    public static double wrist = 0;
+    public static double arm = 0;
+
+    public static double linkage = 0;
+
+    public static double drone = 0;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Robot r = new Robot(hardwareMap, telemetry);
+
+        while(opModeIsActive()){
+            r.d.setWrist(wrist);
+            r.d.setArm(arm);
+            r.d.setClawL(clawL);
+            r.d.setClawR(clawR);
+
+            r.i.setLinkage(linkage);
+
+            r.dr.setDrone(drone);
+
+        }
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/SlidesTuner.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.opmodes.tuning;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.subsystem.deposit.Slides;
+
+
+@TeleOp
+@Config
+public class SlidesTuner extends LinearOpMode {
+
+    public static double goalPos = 0;
+
+    public void runOpMode() throws InterruptedException {
+        Slides s = new Slides(hardwareMap);
+
+        Telemetry t = new MultipleTelemetry(FtcDashboard.getInstance().getTelemetry(), telemetry);
+
+        while(opModeIsActive()){
+            s.setPidTarget(goalPos);
+
+            t.addData("Goal pos", goalPos);
+            t.addData("Current pos", s.getPos());
+        }
+    }
+}
